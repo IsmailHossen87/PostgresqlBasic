613bf285a5359bc3396897679db264492ad508b1
Ismail <ismailhosen8757@gmail.com>
2025-05-20 00:17:03 +0600

    addComment

--- Practice.sql
+++ Practice.sql
@@ -7,7 +7,7 @@ CREATE Table student(
     email VARCHAR(50) ,
     class VARCHAR(20)
 )
-
+-- delete the table
 DROP Table student;
 
 INSERT INTO student(name, country, roll, age, email, class) VALUES
@@ -27,37 +27,37 @@ INSERT INTO student(name, country, roll, age, email, class) VALUES
 ('Asha', 'India', 114, 19, 'asha@example.com', 'six'),
 ('Nayan', 'Pakistan', 115, 20, 'nayan@example.com', 'eight');
 
-
+-- delete for tables value
 DELETE FROM student;
-
+-- add a new Column
 ALTER Table student ADD COLUMN class  VARCHAR(20);
-
+-- show a specific column
 SELECT email as "Student_email" FROM student;
-
+-- To see age assencronouns or DESC
 SELECT * FROM student ORDER BY age DESC;
-
+-- ITS to see Uniqe age /Similar age
 SELECT DISTINCT age FROM student ORDER BY age ASC;
-
+-- use and opatetor and Or oparetor
 SELECT * FROM student
     WHERE age = 20 OR country = 'Bangladesh';
-
+-- its do to lower case a column
 SELECT lower(name)AS student_name  FROM student;
 SELECT INITCAP(name) AS formatted_name FROM student;
 
-
 SELECT * FROM student   
     WHERE  country = 'Nepal'
-
+-- where email is null then its set a default value
 SELECT COALESCE(email,'DEFAULTeamil@gamil.com') FROM student;
-
+-- IN its use to safe OR oparetor
 SELECT * FROM student
     WHERE age IN (20,21) ORDER BY age ASC ;
 
--- IMPORTANT START
+-- To see similar total age
 SELECT age,count(*) AS total_students FROM student
     WHERE age IN (20,21) GROUP BY age;
 -- IMPORTANT end
 
+-- Between
 SELECT * FROM student
     WHERE age BETWEEN 19 AND 21 ORDER BY age ASC;
 
@@ -72,8 +72,6 @@ SELECT * FROM student ORDER BY id LIMIT 5 OFFSET 5 * 1;
 SELECT * FROM student ORDER BY id LIMIT 5 OFFSET 10;
 -- Pagination END
 SELECT * FROM student;
-
+-- Update
 UPDATE student set email = 'defaultNewEmail@gmail.com',age = 21
     WHERE id = 16;
-INSERT INTO student(name, country, roll, age, email, class)
-VALUES ('Sokhina', 'USA', 117, 20,NULL , 'six');
